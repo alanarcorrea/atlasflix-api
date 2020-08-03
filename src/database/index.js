83dc4aca837fadb5bf59c70b8a037b39fc0a8280
Alana Corrêa <alanarcorrea.atlas@gmail.com>
2020-08-02 22:06:37 -0300

    mongo uri env

--- src/database/index.js
+++ src/database/index.js
@@ -2,14 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-function host() {
-  // if (process.env.PRODUCTION == 'true') {
-  // }
-  // return "mongodb://localhost/db-atlasflix";
-  return "mongodb://heroku_r1t7bkpk:316me9gmnrto21ltppn2v92gs@ds043358.mlab.com:43358/heroku_r1t7bkpk";
-}
-
-mongoose.connect(host(), {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
