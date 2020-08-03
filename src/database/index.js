15e1e73e0855f3a533089ef05e7d02dc3f529bb3
Alana Corrêa <alanarcorrea.atlas@gmail.com>
2020-08-02 22:00:48 -0300

    tyr out

--- src/database/index.js
+++ src/database/index.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 function host() {
-  if (process.env.PRODUCTION == 'true') {
-    return "mongodb://heroku_r1t7bkpk:316me9gmnrto21ltppn2v92gs@ds043358.mlab.com:43358/heroku_r1t7bkpk";
-  }
-  return "mongodb://localhost/db-atlasflix";
+  // if (process.env.PRODUCTION == 'true') {
+  // }
+  // return "mongodb://localhost/db-atlasflix";
+  return "mongodb://heroku_r1t7bkpk:316me9gmnrto21ltppn2v92gs@ds043358.mlab.com:43358/heroku_r1t7bkpk";
 }
 
 mongoose.connect(host(), {
